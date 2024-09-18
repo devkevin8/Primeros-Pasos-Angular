9dcb3329397a01fe49ff3f129a352208bdb7c54e
devkevin6 <kevin.rosales50@gmail.com>
2024-09-18 12:23:25 -0600

    primera subida

---
+++ src/app/user2/user2.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user2',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      user2 works!
+    </p>
+  `,
+  styles: ``
+})
+export class User2Component {
+
+}
---
+++ src/app/user3/user3.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user3',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      user3 works!
+    </p>
+  `,
+  styles: ``
+})
+export class User3Component {
+
+}
